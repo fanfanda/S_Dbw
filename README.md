5feb486ff9772d16ca2342db136e32f3a02a582f
fanfanda <fanfanda@ict.ac.cn>
2018-11-25 10:06:17 +0800

    nothing

--- README.md
+++ README.md
@@ -2,9 +2,16 @@
 S_Dbw validity index.
 If you think the code is useful,please give me a star ^_^!
 
+## description
+The S_Dbw implemented here is suitable for evaluating the k-medoids clustering algorithm. The clustering center of the algorithm is different from k-means, which is a specific point. Therefore, some people want to use other clustering algorithms to evaluate, and need to modify the code slightly.
+
+## S_Dbw
+S_Dbw consists of two items, inter-cluster density($1.1$) and intra-cluster variance($1.2$). When using it to select the superparameter of the clustering algorithm, we will choose a set of parameters that minimize the S_Dbw value.
+
+$$Dens\_bw(c) = \frac{1}{c\cdot(c-1)}\sum_{i=1}^{n}(\sum_{{{j=1}\atop{i\neq{j}}}}^{c}\frac{density(u_{ij})}{\max(density(v_i), density(v_j))}) \tag{1.1}$$
+
+$$Scat(c) = \frac{1}{c}\sum_{i=1}^{c}\frac{\left\|\delta(v_i)\right\|}{\left\|\delta(S)\right\|} \tag{1.2}$$
+
 ## paper link
 &nbsp; [Clustering Validity Assessment: Finding the optimal partitioning of a data set](https://pdfs.semanticscholar.org/dc44/df745fbf5794066557e52074d127b31248b2.pdf) </br>
 &nbsp; [Understanding of Internal Clustering Validation Measures](http://datamining.rutgers.edu/publication/internalmeasures.pdf)
-
-## description
-The S_Dbw implemented here is suitable for evaluating the k-medoids clustering algorithm. The clustering center of the algorithm is different from k-means, which is a specific point. Therefore, some people want to use other clustering algorithms to evaluate, and need to modify the code slightly.
--- S_Dbw.py
+++ S_Dbw.py
@@ -2,35 +2,37 @@ import numpy as np
 import math
 
 class S_Dbw():
-    def __init__(self,data,data_cluster,centers_id):
+    def __init__(self,data, data_cluster, centers_index):
         """
         data --> raw data
-        data_cluster --> The category that represents each piece of data(the number of category should begin 0)
-        centers_id --> the center_id of each cluster's center
+        data_cluster --> The category to which each point belongs(category starts at 0)
+        centers_index --> Cluster center index
         """
         self.data = data
         self.data_cluster = data_cluster
-        self.centers_id = centers_id
+        self.centers_index = centers_index
 
-        self.k = len(centers_id)
+        self.k = len(centers_index)
+        
+        # Initialize stdev
         self.stdev = 0
         for i in range(self.k):
-            std_matrix_i = np.std(data[self.data_cluster == i],axis=0)
-            self.stdev += math.sqrt(np.dot(std_matrix_i.T,std_matrix_i))
+            std_matrix_i = np.std(data[self.data_cluster == i], axis = 0)
+            self.stdev += math.sqrt(np.dot(std_matrix_i.T, std_matrix_i))
         self.stdev = math.sqrt(self.stdev)/self.k
         
 
-    def density(self,density_list=[]):
+    def density(self, density_list = []):
         """
         compute the density of one or two cluster(depend on density_list)
         """
         density = 0
-        centers_id1 = self.centers_id[density_list[0]]
+        centers_index1 = self.centers_index[density_list[0]]
         if len(density_list) == 2:
-            centers_id2 = self.centers_id[density_list[1]]
-            center_v = (self.data[centers_id1] + self.data[centers_id2])/2
+            centers_index2 = self.centers_index[density_list[1]]
+            center_v = (self.data[centers_index1] + self.data[centers_index2])/2
         else:
-            center_v = self.data[centers_id1]
+            center_v = self.data[centers_index1]
         for i in density_list:
             temp = self.data[self.data_cluster == i]
             for j in temp:
@@ -42,35 +44,35 @@ class S_Dbw():
         density_list = []
         result = 0
         for i in range(self.k):
-            density_list.append(self.density(density_list=[i]))
+            density_list.append(self.density(density_list = [i]))
         for i in range(self.k):
             for j in range(self.k):
-                if i==j:
+                if i == j:
                     continue
-                result += self.density([i,j])/max(density_list[i],density_list[j])
-        return result/(self.k*(self.k-1))
+                result += self.density([i,j])/max(density_list[i], density_list[j])
+        return result/(self.k*(self.k - 1))
     
     def Scat(self):
-        theta_s = np.std(self.data,axis=0)
-        theta_s_2norm = math.sqrt(np.dot(theta_s.T,theta_s))
+        theta_s = np.std(self.data, axis = 0)
+        theta_s_2norm = math.sqrt(np.dot(theta_s.T, theta_s))
         sum_theta_2norm = 0
         
         for i in range(self.k):
             matrix_data_i = self.data[self.data_cluster == i]
-            theta_i = np.std(matrix_data_i,axis=0)
-            sum_theta_2norm += math.sqrt(np.dot(theta_i.T,theta_i))
-        return sum_theta_2norm/(theta_s_2norm*self.k)
+            theta_i = np.std(matrix_data_i, axis = 0)
+            sum_theta_2norm += math.sqrt(np.dot(theta_i.T, theta_i))
+        return sum_theta_2norm/(theta_s_2norm * self.k)
 
     def S_Dbw_result(self):
         """
         compute the final result
         """
-        return self.Dens_bw()+self.Scat()
+        return self.Dens_bw() + self.Scat()
 
 #just for tests
 data = np.array([[1,2,1],[0,1,4],[3,3,3],[2,2,2]])
 data_cluster = np.array([1,0,1,2]) # The category represents each piece of data belongs
-centers_id = np.array([1,0,3]) # the cluster's num is 3
+centers_index = np.array([1,0,3]) # the cluster's num is 3
 
-a = S_Dbw(data,data_cluster,centers_id)
+a = S_Dbw(data,data_cluster,centers_index)
 print(a.S_Dbw_result())
